64f0bc20736b20d5806766989f574258159431db
AlbertAartsen <albertusaartsen@gmail.com>
2017-10-05 17:24:53 +0200

    pizza dillivery

---
+++ pizza_dil.rb
@@ -0,0 +1,27 @@
+class Menu
+  attr_accessor
+
+ def initialize
+
+ end
+
+
+end
+
+class Pizza
+   attr_accessor
+
+ def initialize
+
+ end
+
+end
+
+class Order
+  attr_accessor
+
+  def initialize
+
+  end
+
+end
